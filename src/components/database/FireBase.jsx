071bf726ccb28fc450ea19b769378417775ff193
Syrymbek Zhumazhan <75174477+srmbackisdeveloper@users.noreply.github.com>
2023-02-20 07:49:26 +0600

    first commit

--- src/components/database/FireBase.jsx
+++ src/components/database/FireBase.jsx
@@ -1,15 +1,17 @@
 import { getFirestore } from '@firebase/firestore';
 import { initializeApp } from 'firebase/app';
 //require('dotenv').config();
+const API_KEY = "AIzaSyBEUBgLAMVCeFcx90Ty3WAztJKTia_z3vQ"
+const APP_ID = "1:1065349031236:web:81cca4f57961efc026a311"
 
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
+    apiKey: API_KEY,
     authDomain: "salarydb-8f14b.firebaseapp.com",
     databaseURL: "https://salarydb-8f14b-default-rtdb.firebaseio.com",
     projectId: "salarydb-8f14b",
     storageBucket: "salarydb-8f14b.appspot.com",
     messagingSenderId: "1065349031236",
-    appId: process.env.APP_ID
+    appId: APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
